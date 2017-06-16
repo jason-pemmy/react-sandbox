5d9a7b60a45a14e7ec932686e0f4e2a9ce745fdf
Jason <jason.pemmy@gmail.com>
2017-06-16 08:15:04 -0400

    learning

--- src/assets/components/about.js
+++ src/assets/components/about.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {TweenMax} from "gsap";
 import '../css/about.min.css';
 
 class FeatureTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            node: {}
+        }        
+    }
+
+    componentDidMount() {
+        var node = ReactDOM.findDOMNode(this);
+        this.setState({node: node});        
+    }
+
     render() { 
-        console.log("===: "+ this.props.imgUrl);
-        const style = {backgroundImage: "url("+this.props.imgUrl+")"};
+        //console.log("===: "+ this.props.imgUrl);
+        const style = {backgroundImage: "url("+this.props.imgUrl+")"}
+        TweenMax.to(this.state.node, 1, {rotation:360, y:100}, 0.5);
         
         return(
             <div className="tile-container">
-                <div className="img-container" style={style}></div>
-                <div className="img-overlay">
-                    <p>{this.props.title}</p> 
+                <div className="img-container" style={style}>
+                    <div className="img-overlay">
+                        <p>{this.props.title}</p> 
+                    </div>
                 </div>
+                
             </div>
         );
     }
@@ -35,8 +52,12 @@ class ProductFeatureContainer extends Component {
     }
 
     render() {    
-        const tileData = this.state.apiData.slice(0,3).map((data) =>
-            <FeatureTile imgUrl={data.url} title={data.title} key={data.id}/>
+        const tileData = this.state.apiData.slice(0,20).map((data) =>
+            <FeatureTile 
+                imgUrl={data.url} 
+                title={data.title} 
+                key={data.id} 
+            />
         );
 
         return(
